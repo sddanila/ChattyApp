aa70d80a8c12a2669c681eb13d2ae8b8c80323bd
sddanila <sddanila@yahoo.co.uk>
2019-03-12 15:09:39 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx';
+import Messages from './Messages.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <NavBar />
+        <Messages />
+        <ChatBar />
+      </div>
+
     );
   }
 }
 export default App;
+
+
+
+
