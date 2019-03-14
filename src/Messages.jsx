2a24d69bfd041af05fdc042965450a04540e7b74
sddanila <sddanila@yahoo.co.uk>
2019-03-14 16:23:51 +0000

    add basic styling to CHattyApp

--- src/Messages.jsx
+++ src/Messages.jsx
@@ -17,7 +17,7 @@ export default class Message extends Component {
           )
         } else if (message.type === 'incomingNotification'){
           return (
-            <div className="notification" key={message.id}>
+            <div style={{backgroundColor: alternatingColor}} className="notification" key={message.id}>
               <span className="notification-content"><em>{message.content}</em></span>
             </div>
           )
